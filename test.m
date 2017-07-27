b93b195d40abf04cea399eece03a52647b144d9e
sumo1988 <30501875+sumo1988@users.noreply.github.com>
2017-07-27 10:41:55 -0400

    Add files via upload

---
+++ test.m
@@ -0,0 +1,124 @@
+function varargout = test(varargin)
+% TEST MATLAB code for test.fig
+%      TEST, by itself, creates a new TEST or raises the existing
+%      singleton*.
+%
+%      H = TEST returns the handle to a new TEST or the handle to
+%      the existing singleton*.
+%
+%      TEST('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TEST.M with the given input arguments.
+%
+%      TEST('Property','Value',...) creates a new TEST or raises
+%      the existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before test_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to test_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help test
+
+% Last Modified by GUIDE v2.5 25-Jul-2017 15:05:15
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @test_OpeningFcn, ...
+                   'gui_OutputFcn',  @test_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before test is made visible.
+function test_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to test (see VARARGIN)
+
+% Choose default command line output for test
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+% UIWAIT makes test wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = test_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in calculate.
+function calculate_Callback(hObject, eventdata, handles)
+% hObject    handle to calculate (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in reset.
+function reset_Callback(hObject, eventdata, handles)
+% hObject    handle to reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+close(gcbf)
+run test
+
+% --- Executes on button press in LoadImage.
+function LoadImage_Callback(hObject, eventdata, handles)
+[filename pathname] = uigetfile({'*.jpg';'*.bmp'},'File Selector');
+image = strcat(pathname, filename);
+% axes(handles.axes1);
+imshow(image)
+% d = dialog ( 'windowstyle', 'normal', 'resize', 'on' );
+% uicontrol ( 'parent', d, 'style', 'pushbutton', 'units', 'normalized', 'position', [0.1 0.1 0.5 0.5], 'string', 'test', 'fontunits', 'normalized', 'fontsize', 0.1 );
+% set(handles.edit1,'string',filename);
+% set(handles.edit2,'string',image);
+% h=imread(filename);
+% image(h);
+% hObject    handle to LoadImage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% --- Executes on button press in ModifyImage.
+function ModifyImage_Callback(hObject, eventdata, handles)
+% hObject    handle to ModifyImage (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in Measure.
+function Measure_Callback(hObject, eventdata, handles)
+% hObject    handle to Measure (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in SaveandExport.
+function SaveandExport_Callback(hObject, eventdata, handles)
+% hObject    handle to SaveandExport (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
